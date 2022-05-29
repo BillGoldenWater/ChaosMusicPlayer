c28b596b8ecc461e43441b6c3191b3db8951415c
BiliGoldenWater <2439577268@qq.com>
2022-05-30 05:50:49 +0800

    完成生成 (能缩放到所有需要的频率(*0.5~*2.0)) 的所有频率

--- src/main/kotlin/indi/goldenwater/chaosmusicplayer/ChaosMusicPlayer.kt
+++ src/main/kotlin/indi/goldenwater/chaosmusicplayer/ChaosMusicPlayer.kt
@@ -8,7 +8,6 @@ import javax.sound.sampled.AudioFileFormat
 import javax.sound.sampled.AudioFormat
 import javax.sound.sampled.AudioInputStream
 import javax.sound.sampled.AudioSystem
-import kotlin.math.PI
 import kotlin.math.roundToInt
 import kotlin.math.sin
 
@@ -22,28 +21,51 @@ class ChaosMusicPlayer : JavaPlugin() {
     }
 }
 
-fun sineWave(frequency: Int, seconds: Int, sampleRate: Int): ByteArray {
+fun sineWave(frequency: Double, seconds: Int, sampleRate: Int): ByteArray {
     val samples = seconds * sampleRate
     val result = ByteBuffer.allocate(samples * Short.SIZE_BYTES)
+
     val interval = sampleRate.toDouble() / frequency
+
     for (i in 0 until samples) {
-        val angle = 2.0 * PI * i / interval
-        result.putShort((sin(angle) * Short.MAX_VALUE).roundToInt().toShort())
+        val radians = Math.toRadians((i / interval) * 360)
+        val value = sin(radians) * Short.MAX_VALUE
+        result.putShort(value.roundToInt().toShort())
     }
     return result.array()
 }
 
-fun generateSineWaveFile(fileName: String, frequency: Int, seconds: Int = 1, sampleRate: Int = 44000) {
+fun generateSineWaveFile(file: File, frequency: Double, seconds: Int = 1, sampleRate: Int = 44000) {
     val buffer = sineWave(frequency, seconds, sampleRate)
     val format = AudioFormat(sampleRate.toFloat(), Short.SIZE_BITS, 1, true, true)
 
     AudioSystem.write(
         AudioInputStream(ByteArrayInputStream(buffer), format, buffer.size.toLong() / Short.SIZE_BYTES),
         AudioFileFormat.Type.WAVE,
-        File(fileName)
+        file
     )
 }
 
+fun generateSineWaveFiles(outputDir: File = File("sineWaveFiles"), maxFrequency: Double = 20000.0) {
+    val startFrequency = 0.5
+
+    var generateFrequency = startFrequency * 2
+    var nextStartFrequency = generateFrequency * 2
+
+    if (!outputDir.exists()) {
+        outputDir.mkdir()
+    } else if (!outputDir.isDirectory) {
+        throw IllegalArgumentException("Output directory is not a directory")
+    }
+
+    while (nextStartFrequency < maxFrequency) {
+        generateSineWaveFile(File(outputDir, "$generateFrequency.wav"), generateFrequency, 1)
+        generateFrequency = nextStartFrequency * 2
+        nextStartFrequency = generateFrequency * 2
+    }
+    generateSineWaveFile(File(outputDir, "$generateFrequency.wav"), generateFrequency, 1)
+}
+
 fun main() {
-    generateSineWaveFile(fileName = "test.wav", frequency = 420, 1)
+    generateSineWaveFiles()
 }
