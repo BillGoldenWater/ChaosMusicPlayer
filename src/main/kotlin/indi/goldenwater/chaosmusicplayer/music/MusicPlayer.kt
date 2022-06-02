e74e7a5efaee3fc1c75cc89648f3f4915e70becb
BiliGoldenWater <2439577268@qq.com>
2022-06-03 07:23:40 +0800

    添加版权

--- src/main/kotlin/indi/goldenwater/chaosmusicplayer/music/MusicPlayer.kt
+++ src/main/kotlin/indi/goldenwater/chaosmusicplayer/music/MusicPlayer.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2021-2022 Golden_Water
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 package indi.goldenwater.chaosmusicplayer.music
 
 import indi.goldenwater.chaosmusicplayer.ChaosMusicPlayer
